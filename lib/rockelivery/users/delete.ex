05c9d44ba4f1dc2f56eaa19bc756c0eaa2246db0
eltoncampos1 <eltoncampos36@gmail.com>
2022-02-28 20:37:25 -0300

    refactor: add module to build errors to handle with them

--- lib/rockelivery/users/delete.ex
+++ lib/rockelivery/users/delete.ex
@@ -1,17 +1,17 @@
 defmodule Rockelivery.Users.Delete do
   alias Ecto.UUID
-  alias Rockelivery.{Repo, User}
+  alias Rockelivery.{Error, Repo, User}
 
   def call(id) do
     case UUID.cast(id) do
-      :error -> {:error, %{status: :bad_request, result: "Invalid format!."}}
+      :error -> {:error, Error.build_id_format_error()}
       {:ok, uuid} -> delete(uuid)
     end
   end
 
   def delete(id) do
     case Repo.get(User, id) do
-      nil -> {:error, %{status: :not_found, result: "User not found!."}}
+      nil -> {:error, Error.build_not_found_user_error()}
       user -> Repo.delete(user)
     end
   end
