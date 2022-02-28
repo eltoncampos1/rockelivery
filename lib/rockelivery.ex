979e0351262332e7cf8e1a2a1e03940fac1685bc
eltoncampos1 <eltoncampos36@gmail.com>
2022-02-28 20:08:02 -0300

    feat: create delete user function

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,7 +1,9 @@
 defmodule Rockelivery do
   alias Rockelivery.Users.Create, as: UserCreate
   alias Rockelivery.Users.Get, as: UserGet
+  alias Rockelivery.Users.Delete, as: UserDelete
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate delete_user(id), to: UserDelete, as: :call
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
 end
