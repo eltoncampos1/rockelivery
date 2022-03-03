64980a6fc24f76e8e23a427ed29f36682c5b8e90
eltoncampos1 <eltoncampos36@gmail.com>
2022-03-03 15:26:18 -0300

    feat: create route, controller and view to create items

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -4,8 +4,12 @@ defmodule Rockelivery do
   alias Rockelivery.Users.Delete, as: UserDelete
   alias Rockelivery.Users.Update, as: UserUpdate
 
+  alias Rockelivery.Items.Create, as: ItemCreate
+
   defdelegate create_user(params), to: UserCreate, as: :call
   defdelegate delete_user(id), to: UserDelete, as: :call
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
   defdelegate update_user(params), to: UserUpdate, as: :call
+
+  defdelegate create_item(params), to: ItemCreate, as: :call
 end
--- lib/rockelivery/item.ex
+++ lib/rockelivery/item.ex
@@ -1,4 +1,5 @@
 defmodule Rockelivery.Item do
+  use Ecto.Schema
   import Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: true}
@@ -23,6 +24,6 @@ defmodule Rockelivery.Item do
     |> cast(params, @required_params)
     |> validate_required(@required_params)
     |> validate_length(:description, min: 6)
-    |> validate_number(:price, greather_than: 0)
+    |> validate_number(:price, greater_than: 0)
   end
 end
